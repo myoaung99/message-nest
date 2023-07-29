356ef5aa9166303a7713fa38c20e4c8a7489ac0a
myoaung99 <myomyintaung064@gmail.com>
2023-07-29 12:44:52 +0630

    added dummy routes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
   getmessges() {
-    return ['messages'];
+    console.log('list all messages');
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log('getMessage => id: ' + id);
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log('createMessage => body: ' + body);
   }
 }
